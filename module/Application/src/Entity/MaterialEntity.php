e09d6b806cefe86e53899fd3fe8aa66f0cc91b40
Blup1980 <Blup1980@users.noreply.github.com>
2017-03-03 09:16:29 +0100

    added MaterialEntity class

---
+++ module/Application/src/Entity/MaterialEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Application\Entity;
+
+/**
+ * Description of MaterialEntity
+ *
+ * @author xraemy
+ */
+class MaterialEntity {
+    //put your code here
+    
+}
